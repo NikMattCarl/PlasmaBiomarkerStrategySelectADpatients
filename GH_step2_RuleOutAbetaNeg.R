4d3e4af49d4b1ad8945ac316d7b2e8039f5e518a
NikMattCarl <145451732+NikMattCarl@users.noreply.github.com>
2023-09-19 15:49:12 +0200

    GH_step2_RuleOutAbetaNeg.R

--- GH_step2_RuleOutAbetaNeg.R
+++ GH_step2_RuleOutAbetaNeg.R
@@ -5,6 +5,7 @@ temp_test=na.omit(test_set[,c("sid","age","gender","apoe4","AB_status",plasma_ma
 
 biomarkers_roc=biomarkers_roc_test=m=list()
 
+#Data frame to store results for models to predict AB status
 results_AB=data.frame(biomarker=character(),
                        AUC_cv10_train=numeric(),
                        train_lw95=numeric(),
@@ -13,21 +14,25 @@ results_AB=data.frame(biomarker=character(),
                        test_lw95=numeric(),
                        test_hi95=numeric())
 
+#Set train control
 trControl <- trainControl(method = "cv", number = 10, classProbs = TRUE,savePredictions = TRUE,
                           summaryFunction = twoClassSummary)
 
+#Loop through individual biomarkers
 for (i in plasma_markers_log10) {
   temp_train$biomarker=temp_train[,i]
   temp_test$biomarker=temp_test[,i]
- 
+
+  #logistic regression models
   m[[i]] <- train(AB_status~., data = temp_train[,c("AB_status","biomarker","age","gender","apoe4")], 
                                         method = "glm", trControl=trControl,tuneLength = 10,
                                         metric="ROC", preProcess=c("center","scale"))
-  
+
+  #Performance in training set
   AUC_cv10= m[[i]]$results$ROC
   ROCSD=m[[i]]$results$ROCSD
   
-  #Performance in training set
+  #ROC curve in training set
   temp_train$preds_caret = as.numeric(unlist(predict(m[[i]], temp_train, type="prob")["AB_Pos"]))
   biomarkers_roc[[i]]=pROC::roc(temp_train$AB_status~temp_train$preds_caret, ci=TRUE)
 
@@ -43,9 +48,9 @@ for (i in plasma_markers_log10) {
 #Train random forest model
 rf1 <- train(AB_status~., data = temp_train[,c("AB_status",plasma_markers_log10,"age","gender","apoe4")], 
              method = "rf",   trControl=trControl, tuneLength = 10, metric="ROC")
-final_mtry=rf1$finalModel$mtry
+final_mtry=rf1$finalModel$mtry #tuning of mytry provided by caret
 
-#manual tuning of n_tree
+#tuning of n_tree
 model_list = list()
 tunegrid = expand.grid(.mtry = final_mtry)
 control = trainControl(method="repeatedcv", number=3, repeats=10, search="grid")
@@ -59,8 +64,7 @@ for (n_tree in c(100, 500, 1000, 1500)) {
   key = toString(n_tree)
   model_list[[key]] = fit
 }
-
-results <- resamples(model_list)
+results = resamples(model_list)
 
 #refit with tuned hyperparameters
 rf1 = train(AB_status~., data=temp_train[,c("AB_status",plasma_markers_log10,"age","gender","apoe4")], 
@@ -71,14 +75,14 @@ rf1 = train(AB_status~., data=temp_train[,c("AB_status",plasma_markers_log10,"ag
 auc_rf1=rf1$results[rf1$results$mtry==final_mtry,"ROC"]
 sd_rf1=rf1$results[rf1$results$mtry==final_mtry,"ROCSD"]
 
-#performance in test data (as shown above)
+#check performance in test data (as shown above for individual biomarkers)
 #store results in results_AB
 
 #extreme gradient boosting
 xgb1 <- train(AB_status~., data =  temp_train[,c("AB_status",plasma_markers_log10,"age","gender","apoe4")],  
               method = "xgbTree",    trControl=trControl,verbosity=0, metric="ROC",
               preProcess = c("center","scale"))
-besttune=xgb1$bestTune
+besttune=xgb1$bestTune #by tuning
 
 #performance in training data
 xgb_auc_cv10=xgb1$results[xgb1$results$eta==besttune$eta &
@@ -113,21 +117,20 @@ svm_auc_sd=svm_model$results[svm_model$results$sigma==besttune$sigma &
 #performance in test data (as shown above)
 #store results in results_AB
 
-#Comparison of test AUC to p-tau217 alone
+#Compare each model to the p-tau217 model
 pvals=NULL
 for (i in results_AB$biomarker) {
   roc_compare=roc.test(biomarkers_roc_test[["PL_ptau217"]],
            biomarkers_roc_test[[i]],
            methods="delong",
            paired=TRUE)  
-  pvals=c(pvals,roc_compare$p.value)
+  pvals=c(pvals,roc_compare$p.value) #pvalues for comparison of ROC curves
 }
-results_AB$pval_comp_ptau217_test=pvals
-results_AB=results_AB[order(results_AB$AUC_cv10_train, decreasing = TRUE),]
+#store the p-values together with results_AB
 
-#now compare with just P-tau217, without covariates
+#Compare models with a sparse P-tau217 model, without covariates
 m_basic <- train(AB_status~PL_ptau217,
-                 data = temp_train[,c("AB_status","PL_ptau217","age","gender","apoe4")], 
+                 data = temp_train[,c("AB_status","PL_ptau217","age","gender","apoe4")], #ensure that the same dataset is used
                  method = "glm", trControl=trControl,tuneLength = 10, metric="ROC")
 
 #Performance in training set
@@ -142,22 +145,20 @@ for (i in results_AB$biomarker) {
               biomarkers_roc_test[[i]],
               methods="delong",
               paired=TRUE)  
-  pvals=c(pvals,roc_compare$p.value)
+  pvals=c(pvals,roc_compare$p.value) #pvalues for comparison of ROC curves
 }
-results_AB$pval_comp_ptau217_basic_test=pvals
-results_AB=results_AB[order(results_AB$AUC_cv10_train, decreasing = TRUE),]
+#store the p-values together with results_AB
 
 
 #
 #Performance of P-tau217 to rule out AB-negatives
 #
 
+#refit model on whole train_set
 m=glm(AB_status~PL_ptau217, data=train_set, family="binomial")
 train_set$preds=predict(m, newdata=train_set, type="response")
 
-#
-#one cutpoint approach - Youden
-#
+#start with a "one cutpoint" approach - Youden
 
 opt_cut_youden <- cutpointr(train_set, PL_ptau217, AB_status,
                         direction = ">=", pos_class = "AB_Pos",
@@ -165,8 +166,8 @@ opt_cut_youden <- cutpointr(train_set, PL_ptau217, AB_status,
                         metric = youden,
                         boot_runs=1000)
 youden_cp=opt_cut_youden$optimal_cutpoint
-boot_ci(opt_cut_youden, sensitivity)
-boot_ci(opt_cut_youden, specificity)
+boot_ci(opt_cut_youden, sensitivity) #get 95% sens
+boot_ci(opt_cut_youden, specificity) #get 95% spec
 
 #performance of youden cutpoint in test set
 test_set$Predicted_AB_class=ifelse(test_set$PL_ptau217>youden_cp,"AB_Pos",
@@ -195,7 +196,7 @@ boot_sens_ci_test=quantile(boot_results_test$sens, probs=c(0.025,0.975))
 boot_spec_ci_test=quantile(boot_results_test$spec, probs=c(0.025,0.975))
 
 #
-#gray zone approach
+#gray zone approach, 2 cutpoints
 #
 
 #evaluate gray zone for plasma ptau217 vs amyloid PET
@@ -206,7 +207,7 @@ m=glm(AB_status~PL_ptau217, family="binomial", data=temp_train)
 temp_train$preds=predict(m, temp_train, type="response")
 temp_test$preds=predict(m, temp_test, type="response")
 
-#mean 90% pos prob
+#procedure to select cut-points for 2-cutpoint approach, based on finding cutpoints which minimizes gray zone while keeping sens and spec>90%
 results=data.frame(cutoff=numeric(),
                    pos_prob=numeric(),
                    neg_prob=numeric())
@@ -248,7 +249,7 @@ results_cupoint=data.frame(pos_neg_prob=numeric(),
                            range_gz_test=numeric(),
                            range_test_lw95=numeric(),
                            range_test_hi95=numeric())
-probs_try=seq(from=0.85,to=0.95, by=0.01)
+probs_try=seq(from=0.85,to=0.95, by=0.01) #iterate through possible levels of probability
 for (k in 1:length(probs_try)) {
   
   current_prob=probs_try[k]
@@ -264,42 +265,14 @@ for (k in 1:length(probs_try)) {
   low_cutpoint=maxneg_group[1,"cutoff"]
   
   #use optimal cutpoints to predict AB class, for carrying forward a filtered data set
-  train_set$Predicted_AB_class=ifelse(train_set$PL_ptau217_pgml_Lilly_2022_log10>=hi_cutpoint,"AB_Pos",
-                                      ifelse(train_set$PL_ptau217_pgml_Lilly_2022_log10<=low_cutpoint,"AB_Neg",
+  train_set$Predicted_AB_class=ifelse(train_set$PL_ptau217>=hi_cutpoint,"AB_Pos",
+                                      ifelse(train_set$PL_ptau217<=low_cutpoint,"AB_Neg",
                                              as.character(train_set$AB_status)))    
   sens_train=table(train_set$Predicted_AB_class, train_set$AB_status)["AB_Pos","AB_Pos"]/sum(table(train_set$Predicted_AB_class, train_set$AB_status)[,"AB_Pos"])
   spec_train=table(train_set$Predicted_AB_class, train_set$AB_status)["AB_Neg","AB_Neg"]/sum(table(train_set$Predicted_AB_class, train_set$AB_status)[,"AB_Neg"])
-  range_gz_train=prop.table(table(train_set$PL_ptau217_pgml_Lilly_2022_log10>low_cutpoint & train_set$PL_ptau217_pgml_Lilly_2022_log10<hi_cutpoint))["TRUE"]
+  range_gz_train=prop.table(table(train_set$PL_ptau217>low_cutpoint & train_set$PL_ptau217<hi_cutpoint))["TRUE"]
   
-  #bootstrap sens and spec
-  #bootstrap for CI
-  boot_results=data.frame(boot=numeric(),
-                          sens_gz=numeric(),
-                          spec_gz=numeric(),
-                          range_gz=numeric())
-  for (i in 1:1000) {
-    boot_id=data.frame(sid=sample(train_set$sid, replace=TRUE), boot_sid=seq(1,nrow(train_set),1))#
-    
-    boot_id=left_join(boot_id, train_set, by="sid")
-    
-    boot_id$Predicted_AB_class=ifelse(boot_id$PL_ptau217>=hi_cutpoint,"AB_Pos",
-                                      ifelse(boot_id$PL_ptau217<=low_cutpoint,"AB_Neg",
-                                             as.character(boot_id$AB_status)))    
-    
-    sens_boot=table(boot_id$Predicted_AB_class,boot_id$AB_status)["AB_Pos","AB_Pos"]/(sum(table(boot_id$Predicted_AB_class,boot_id$AB_status)[,"AB_Pos"]))
-    spec_boot=table(boot_id$Predicted_AB_class,boot_id$AB_status)["AB_Neg","AB_Neg"]/(sum(table(boot_id$Predicted_AB_class,boot_id$AB_status)[,"AB_Neg"]))
-    
-    range_gz_boot=prop.table(table(boot_id$PL_ptau217_pgml_Lilly_2022_log10>low_cutpoint & boot_id$PL_ptau217_pgml_Lilly_2022_log10<hi_cutpoint))["TRUE"]
-    
-    sub=data.frame(boot=i,
-                   sens_gz=sens_boot,
-                   spec_gz=spec_boot,
-                   range_gz=as.numeric(range_gz_boot))
-    boot_results=rbind(boot_results,sub)
-  }
-  boot_sens_ci=quantile(boot_results$sens, probs=c(0.025,0.975), na.rm=TRUE)
-  boot_spec_ci=quantile(boot_results$spec, probs=c(0.025,0.975), na.rm=TRUE)
-  boot_gz=quantile(boot_results$range_gz, probs=c(0.025,0.975), na.rm=TRUE)
+  #bootstrap sens and spec as shown above 
   
   #check performance in the test set
   test_set$Predicted_AB_class=ifelse(test_set$PL_ptau217>=hi_cutpoint,"AB_Pos",
@@ -310,30 +283,8 @@ for (k in 1:length(probs_try)) {
   spec_test=test_table["AB_Neg","AB_Neg"]/sum(test_table[,"AB_Neg"])
   range_gz_test=prop.table(table(test_set$PL_ptau217>low_cutpoint & test_set$PL_ptau217<hi_cutpoint))["TRUE"]
   
-  #bootstrap sens and spec, test set
-  boot_results_test=data.frame(boot=numeric(),
-                               sens_gz=numeric(),
-                               spec_gz=numeric(),
-                               range_gz=numeric())
-  for (i in 1:1000) {
-    boot_id=data.frame(sid=sample(test_set$sid, replace=TRUE), boot_sid=seq(1,nrow(test_set),1))#
-    boot_id=left_join(boot_id, test_set, by="sid")
-    boot_id$Predicted_AB_class=ifelse(boot_id$PL_ptau217>=hi_cutpoint,"AB_Pos",
-                                      ifelse(boot_id$PL_ptau217<=low_cutpoint,"AB_Neg",
-                                             as.character(boot_id$AB_status)))    
-    sens_boot=table(boot_id$Predicted_AB_class,boot_id$AB_status)["AB_Pos","AB_Pos"]/(sum(table(boot_id$Predicted_AB_class,boot_id$AB_status)[,"AB_Pos"]))
-    spec_boot=table(boot_id$Predicted_AB_class,boot_id$AB_status)["AB_Neg","AB_Neg"]/(sum(table(boot_id$Predicted_AB_class,boot_id$AB_status)[,"AB_Neg"]))
-    range_gz_boot=prop.table(table(boot_id$PL_ptau217>low_cutpoint & boot_id$PL_ptau217<hi_cutpoint))["TRUE"]
-    sub=data.frame(boot=i,
-                   sens_gz=sens_boot,
-                   spec_gz=spec_boot,
-                   range_gz=as.numeric(range_gz_boot))
-    boot_results_test=rbind(boot_results_test,sub)
-  }
-  boot_sens_ci_test=quantile(boot_results_test$sens, probs=c(0.025,0.975), na.rm=TRUE)
-  boot_spec_ci_test=quantile(boot_results_test$spec, probs=c(0.025,0.975), na.rm=TRUE)
-  boot_gz_test=quantile(boot_results_test$range_gz, probs=c(0.025,0.975), na.rm=TRUE)
-  
+  #bootstrap sens and spec in test set as shown above 
+
   sub_prob=data.frame(pos_neg_prob=current_prob,
                       high_cutpoint=hi_cutpoint,
                       low_cutpoint=low_cutpoint,
@@ -380,16 +331,18 @@ for (i in 1:nrow(temp_train)) {
                  neg_prob=meannegprob)
   results=rbind(results,sub)
 }
-#hi cutpoint 
+
+#final hi cutpoint 
 pos_range=results[results$pos_prob>0.88,]
 minpos=pos_range[order(pos_range$cutoff, decreasing=FALSE),]
 hi_cutpoint=minpos[1,"cutoff"]
-#low cutpoint
+
+#final low cutpoint
 neg_range=results[results$neg_prob<0.12,]
 maxneg=neg_range[order(neg_range$cutoff, decreasing=TRUE),]
 low_cutpoint=maxneg[1,"cutoff"]
 
-#use optimal cutpoints for gray zone to predict AB class
+#use optimal final cutpoints for gray zone to predict AB class
 train_set$Predicted_AB_class=ifelse(train_set$PL_ptau217>=hi_cutpoint,"AB_Pos",
                                     ifelse(train_set$PL_ptau217<=low_cutpoint,"AB_Neg",
                                            as.character(train_set$AB_status)))    
@@ -397,32 +350,7 @@ sens=table(train_set$Predicted_AB_class, train_set$AB_status)["AB_Pos","AB_Pos"]
 spec=table(train_set$Predicted_AB_class, train_set$AB_status)["AB_Neg","AB_Neg"]/sum(table(train_set$Predicted_AB_class, train_set$AB_status)[,"AB_Neg"])
 range_gz_train=prop.table(table(train_set$PL_ptau217>low_cutpoint & train_set$PL_ptau217<hi_cutpoint))["TRUE"]
 
-#bootstrap sens and spec in training set
-boot_results=data.frame(boot=numeric(),
-                        sens_gz=numeric(),
-                        spec_gz=numeric(),
-                        range_gz=numeric())
-for (i in 1:1000) {
-  boot_id=data.frame(sid=sample(train_set$sid, replace=TRUE), boot_sid=seq(1,nrow(train_set),1))#
-  boot_id=left_join(boot_id, train_set, by="sid")
-  boot_id$Predicted_AB_class=ifelse(boot_id$PL_ptau217>=hi_cutpoint,"AB_Pos",
-                                   ifelse(boot_id$PL_ptau217<=low_cutpoint,"AB_Neg",
-                                           as.character(boot_id$AB_status)))    
-
-  sens_boot=table(boot_id$Predicted_AB_class,boot_id$AB_status)["AB_Pos","AB_Pos"]/(sum(table(boot_id$Predicted_AB_class,boot_id$AB_status)[,"AB_Pos"]))
-  spec_boot=table(boot_id$Predicted_AB_class,boot_id$AB_status)["AB_Neg","AB_Neg"]/(sum(table(boot_id$Predicted_AB_class,boot_id$AB_status)[,"AB_Neg"]))
-  range_gz_boot=prop.table(table(boot_id$PL_ptau217>low_cutpoint & boot_id$PL_ptau217<hi_cutpoint))["TRUE"]
-
-  sub=data.frame(boot=i,
-                 sens_gz=sens_boot,
-                 spec_gz=spec_boot,
-                 range_gz=range_gz_boot)
-  boot_results=rbind(boot_results,sub)
-}
-boot_sens_ci=quantile(boot_results$sens, probs=c(0.025,0.975))
-boot_spec_ci=quantile(boot_results$spec, probs=c(0.025,0.975))
-boot_gz=quantile(boot_results$range_gz, probs=c(0.025,0.975))
-
+#bootstrap sens and spec in training set as shown above
 
 #check performance in the test set
 test_set$Predicted_AB_class=ifelse(test_set$PL_ptau217>=hi_cutpoint,"AB_Pos",
@@ -432,7 +360,8 @@ test_table=table(test_set$Predicted_AB_class,test_set$AB_status)
 sens_test=test_table["AB_Pos","AB_Pos"]/sum(test_table[,"AB_Pos"])
 spec_test=test_table["AB_Neg","AB_Neg"]/sum(test_table[,"AB_Neg"])
 range_gz_test=prop.table(table(test_set$PL_ptau217>low_cutpoint & test_set$PL_ptau217<hi_cutpoint))["TRUE"]
-#bootstrap sens and spec in test set (as shown above)
+
+#bootstrap sens and spec in test set as shown above
 
 
 
